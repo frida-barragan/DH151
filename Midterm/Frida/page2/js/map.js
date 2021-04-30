bfbf628a00f96922eb144ce15c46954afe18f74e
frida-barragan <nicole.barragan@icloud.com>
2021-04-30 09:15:15 -0700

    midterm

--- Midterm/Frida/page2/js/map.js
+++ Midterm/Frida/page2/js/map.js
@@ -113,9 +113,9 @@ function createMap(lat,lon,zl){
 	map = L.map('map').setView([lat,lon], zl
     );
 
-	L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-    }).addTo(map);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
 }
 
 // function to read csv data
