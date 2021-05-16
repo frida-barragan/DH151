f6560dd1143368fbdb03344893ea363b74d25d86
frida-barragan <nicole.barragan@icloud.com>
2021-05-15 23:47:16 -0700

    week7

---
+++ week7/js/map.js
@@ -0,0 +1,97 @@
+// Global variables
+let map;
+let lat = 0;
+let lon = 0;
+let zl = 3;
+let path = '';
+let geojsonPath = 'data/world.json';
+let geojson_data;
+let geojson_layer;
+let brew = new classyBrew();
+let fieldtomap;
+
+// initialize
+$( document ).ready(function() {
+    createMap(lat,lon,zl);
+    getGeoJSON();
+});
+
+// create the map
+function createMap(lat,lon,zl){
+	map = L.map('map').setView([lat,lon], zl);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+// function to get the geojson data
+function getGeoJSON(){
+
+	$.getJSON(geojsonPath,function(data){
+		console.log(data)
+
+		// put the data in a global variable
+		geojson_data = data;
+
+		// call the map function
+		mapGeoJSON('pop_est')
+	})
+}
+
+function mapGeoJSON(field){
+
+	// clear layers in case it has been mapped already
+	if (geojson_layer){
+		geojson_layer.clearLayers()
+	}
+	
+	// globalize the field to map
+	fieldtomap = field;
+
+	// create an empty array
+	let values = [];
+
+	// based on the provided field, enter each value into the array
+	geojson_data.features.forEach(function(item,index){
+		values.push(item.properties[field])
+	})
+
+	// set up the "brew" options
+	brew.setSeries(values);
+	brew.setNumClasses(5);
+	brew.setColorCode('YlOrRd');
+	brew.classify('quantiles');
+
+	// create the layer and add to map
+	geojson_layer = L.geoJson(geojson_data, {
+		style: getStyle //call a function to style each feature
+	}).addTo(map);
+
+	map.fitBounds(geojson_layer.getBounds())
+}
+
+function getStyle(feature){
+	return {
+		stroke: true,
+		color: 'white',
+		weight: 1,
+		fill: true,
+		fillColor: brew.getColorInRange(feature.properties[fieldtomap]),
+		fillOpacity: 0.8
+	}
+}
+
+
+// return the color for each feature
+function getColor(d) {
+
+	return d > 1000000000 ? '#800026' :
+		   d > 500000000  ? '#BD0026' :
+		   d > 200000000  ? '#E31A1C' :
+		   d > 100000000  ? '#FC4E2A' :
+		   d > 50000000   ? '#FD8D3C' :
+		   d > 20000000   ? '#FEB24C' :
+		   d > 10000000   ? '#FED976' :
+					  '#FFEDA0';
+}
